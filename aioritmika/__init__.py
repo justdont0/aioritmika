647f2f2d86f3d39f449e6adf2c52fce48bbfd4e4
justdont0 <97027325+justdont0@users.noreply.github.com>
2023-11-12 21:59:26 +0300

    Day 1

---
+++ aioritmika/__init__.py
@@ -0,0 +1,30 @@
+'''Штука для ботов в алге'''
+from warnings import warn
+import asyncio
+import aiohttp
+
+class Bot:
+    '''
+    Ну да
+    
+    Аргументы init:
+    login - Логин для входа в алгу/имя бота
+    password - Пароль для входа в алгу
+    timeout - Задержка между проверками комментариев
+    '''
+    def __init__(self, login: str, password: str, timeout: float = 5):
+        '''
+        Создать бота
+
+        Аргументы:
+        login - Логин для входа в алгу/имя бота
+        password - Пароль для входа в алгу
+        timeout - Задержка между проверками комментариев
+        '''
+        if type(login) != str:
+            raise TypeError('login is not `str`')
+        if type(password) != str:
+            raise TypeError('password is not `str`')
+        self.login = login
+        self.password = password
+        self.timeout = timeout
