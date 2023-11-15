eccae2b36b9b1fe1827558a2c58ff63c2c801198
justdont0 <97027325+justdont0@users.noreply.github.com>
2023-11-15 15:55:56 +0300

    idek

--- aioritmika/__init__.py
+++ aioritmika/__init__.py
@@ -3,6 +3,7 @@ from typing import Literal
 from warnings import warn
 import asyncio
 import aiohttp
+import json
 
 class Bot:
     '''
@@ -13,8 +14,9 @@ class Bot:
     password - Пароль для входа в алгу
     prefix - Префикс команд
     timeout - Задержка между проверками комментариев
+    storage - Путь к файлу, куда будет сохраняться/загружаться информация об обработанных комментариях (чтобы один комментарий не обпабатывать дважды). Если None, хранит информацию в переменной (сбрасываеться при перезагрузке скрипта)
     '''
-    def __init__(self, login: str, password: str, prefix: str = "", timeout: float = 5):
+    def __init__(self, login: str, password: str, prefix: str = "", timeout: float = 5, storage: str|None = 'aioritmika_data.json'):
         '''
         Создать бота
 
@@ -23,6 +25,7 @@ class Bot:
         password - Пароль для входа в алгу
         prefix - Префикс команд
         timeout - Задержка между проверками комментариев
+        storage - Путь к файлу, куда будет сохраняться/загружаться информация об обработанных комментариях (чтобы один комментарий не обпабатывать дважды). Если None, хранит информацию в переменной (сбрасываеться при перезагрузке скрипта)
         '''
         if type(login) != str:
             raise TypeError('login is not `str`')
@@ -32,10 +35,21 @@ class Bot:
         self.password = password
         self.timeout = timeout
         self.prefix = prefix
+        self.storage = storage
+        if storage is None: self.temp_storage = {}
+        else:
+            try: self.temp_storage = json.load(open(storage, 'r'))
+            except: self.temp_storage = {}
         self.commands = {}
         self.running = False
 
     def command(self, name: str|None = None):
+        '''
+        Декоратор для добавления команды в бота
+
+        Аргументы:
+        name - Имя команды, если None - имя функции
+        '''
         def decor(coro):
             self.commands[self.prefix + (name if name is not None else coro.__name__)] = coro
             return coro
